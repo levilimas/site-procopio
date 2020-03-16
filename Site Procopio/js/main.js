00926e95644cd2714c5a2d0f1bc432875d6704b9
João Emerson <joaoemersonufc@gmail.com>
2020-03-16 17:23:27 -0300

    Limpeza no código; Término da navbar (falta a escolha de cores); Término do banner (falta escolha de cores e imagens).

--- Site Procopio/js/main.js
+++ Site Procopio/js/main.js
@@ -1,20 +1,104 @@
 (function ($) {
 "use strict";
 // TOP Menu Sticky
-$(window).on('scroll', function () {
-	var scroll = $(window).scrollTop();
-	if (scroll < 400) {
-    $("#sticky-header").removeClass("sticky");
-    $('#back-top').fadeIn(500);
-	} else {
-    $("#sticky-header").addClass("sticky");
-    $('#back-top').fadeIn(500);
-	}
-});
+var prevScrollpos = window.pageYOffset;
+
+
+window.onscroll = function() {
+
+    var currentScrollPos = window.pageYOffset;
+
+    if (currentScrollPos > 200) {
+        var height = document.getElementsByClassName("header-area")[0].offsetHeight;
+        $('.header-area').addClass('navbar-dark')
+        // document.getElementsByClassName("header-area")[0].classList.add('navbar-dark');
+        // document.querySelector(".scroll-top").style.display = 'block';
+    }else{
+        $('.header-area').removeClass('navbar-dark')
+        // document.getElementsByClassName("header-area")[0].classList.remove('navbar-dark');
+        // document.querySelector(".scroll-top").style.display = 'none';
+    }
+
+   
+    if (prevScrollpos > currentScrollPos) {
+      // alert(height)
+        // alert("gayyyyyyyyyyyyyyy");
+        
+        document.getElementsByClassName("header-area")[0].style.top = "0";
+    } else {
+        
+        document.getElementsByClassName("header-area")[0].style.top = `-${height}px`;
+    }
+    
+
+    prevScrollpos = currentScrollPos;
+    
+}
+
+var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+        }
+
+        setTimeout(function() {
+        that.tick();
+        }, delta);
+    };
+
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+        document.body.appendChild(css);
+    };
+
 
 
 $(document).ready(function(){
 
+$('.header-area').css({
+  top: '0'
+})
 // mobile_menu
 var menu = $('ul#navigation');
 if(menu.length){
